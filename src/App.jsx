0890aef4207a12d3a453818ef17ffe7f30796c5d
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-12-02 15:58:43 +0100

    Add card to display sprite and name of the sprite

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,34 @@ import { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  async function fetchURL() {
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1025`);
+  return (
+    <div className="card">
+      <img src={fetchURL(4)} alt="Charmander" />
+      <h1>Charmander</h1>
+    </div>
+  );
+}
 
-      if (response.status >= 400 && response.status < 600) {
-        throw new Error('Request had an invalid Id!');
-      }
+// Link selber zusammenbasteln ohne fetch und erst später mit fetch arbeiten, wenn auch behandelt in ODIN
 
-      const {
-        sprites: { front_default },
-      } = await response.json();
+async function fetchURL(id) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/` + id, {
+      mode: 'cors',
+    });
 
-      console.log(front_default);
-    } catch (error) {
-      console.log(error);
-      alert(error);
+    if (response.status >= 400 && response.status < 600) {
+      throw new Error('Request had an invalid Id!');
     }
+
+    const {
+      sprites: { front_default },
+    } = await response.json();
+
+    console.log(front_default);
+    return front_default;
+  } catch (error) {
+    console.log(error);
+    alert(error);
   }
-  fetchURL();
 }
