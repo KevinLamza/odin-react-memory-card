0e0eea1d8570640c9b3dd864f86ba9dba592fc27
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-12-03 13:23:51 +0100

    Add grid to display several cards and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
 import { useState } from 'react';
 import './App.css';
+import fetchURL from './fetchURL';
+import { pokemon } from './pokemon';
 
 export default function App() {
   return (
-    <div className="card">
-      <img src={fetchURL(4)} alt="Charmander" />
-      <h1>Charmander</h1>
-    </div>
+    <>
+      <div className="allCards">
+        <div className="card">
+          <img src={fetchURL(1)} alt={pokemon[1]} />
+          <h1>{pokemon[1]}</h1>
+        </div>
+        <div className="card">
+          <img src={fetchURL(4)} alt={pokemon[4]} />
+          <h1>{pokemon[4]}</h1>
+        </div>
+        <div className="card">
+          <img src={fetchURL(7)} alt={pokemon[7]} />
+          <h1>{pokemon[7]}</h1>
+        </div>
+        <div className="card">
+          <img src={fetchURL(25)} alt={pokemon[25]} />
+          <h1>{pokemon[25]}</h1>
+        </div>
+        <div className="card">
+          <img src={fetchURL(132)} alt={pokemon[132]} />
+          <h1>{pokemon[132]}</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
 // Link selber zusammenbasteln ohne fetch und erst später mit fetch arbeiten, wenn auch behandelt in ODIN
+// ein object wo id als key und name als value drin ist, und mit dieser info über den link die sprites fetchen
 
-async function fetchURL(id) {
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/` + id, {
-      mode: 'cors',
-    });
+// async function fetchURL(id) {
+//   try {
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/` + id, {
+//       mode: 'cors',
+//     });
 
-    if (response.status >= 400 && response.status < 600) {
-      throw new Error('Request had an invalid Id!');
-    }
+//     if (response.status >= 400 && response.status < 600) {
+//       throw new Error('Request had an invalid Id!');
+//     }
 
-    const {
-      sprites: { front_default },
-    } = await response.json();
+//     const {
+//       sprites: { front_default },
+//     } = await response.json();
 
-    console.log(front_default);
-    return front_default;
-  } catch (error) {
-    console.log(error);
-    alert(error);
-  }
-}
+//     console.log(front_default);
+//     return front_default;
+//   } catch (error) {
+//     console.log(error);
+//     alert(error);
+//   }
+// }
+
+// fetchURL(1);
