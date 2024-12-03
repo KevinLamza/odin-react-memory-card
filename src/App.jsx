c3876d0c2cf5410cad66a13b759c35a359dc4b37
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-12-03 14:39:26 +0100

    Add shuffle function to randomize cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,42 @@
 import { useState } from 'react';
 import './App.css';
-import fetchURL from './fetchURL';
 import { pokemon } from './pokemon';
 import { Card } from './Card';
 
 export default function App() {
-  const unshuffledIds = Object.keys(pokemon);
-  const shuffledIds = unshuffledIds
-    .map((value) => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
-  const usedIds = shuffledIds.slice(0, 5);
+  const usedIds = firstSet();
+  const [cardOrder, setCardOrder] = useState(usedIds);
+
+  function handleClick() {
+    // stuff
+  }
 
   return (
     <>
       <div className="appContainer">
         <h1>POKéMON MEMORY GAME</h1>
         <div className="allCards">
-          <Card id={usedIds[0]} />
-          <Card id={usedIds[1]} />
-          <Card id={usedIds[2]} />
-          <Card id={usedIds[3]} />
-          <Card id={usedIds[4]} />
+          <Card id={cardOrder[0]} />
+          <Card id={cardOrder[1]} />
+          <Card id={cardOrder[2]} />
+          <Card id={cardOrder[3]} />
+          <Card id={cardOrder[4]} />
         </div>
       </div>
     </>
   );
+}
+
+function firstSet() {
+  const unshuffledIds = Object.keys(pokemon);
+  const shuffledIds = shuffleSet(unshuffledIds);
+  return shuffledIds.slice(0, 5);
+}
+
+function shuffleSet(unshuffledIds) {
+  const shuffledIds = unshuffledIds
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+  return shuffledIds;
 }
