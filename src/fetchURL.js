7cf5cbaf589c436ae1b17e55813f1550bd26f500
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-12-03 13:23:16 +0100

    Add function to generate sprite img src from id

--- src/fetchURL.js
+++ src/fetchURL.js
@@ -1,19 +1,7 @@
-async function fetchURL() {
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1025`);
-
-    if (response.status >= 400 && response.status < 600) {
-      throw new Error('Request had an invalid Id!');
-    }
-
-    const {
-      sprites: { front_default },
-    } = await response.json();
-
-    console.log(front_default);
-  } catch (error) {
-    console.log(error);
-    alert(error);
-  }
+export default function fetchURL(id) {
+  return (
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/' +
+    id +
+    '.png'
+  );
 }
-fetchURL();
