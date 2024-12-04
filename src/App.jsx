d7cd40090f36422838d6b43a21cd212188879b70
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-12-04 01:07:03 +0100

    Add a difficulty selection before the game

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Card } from './Card';
 
 export default function App() {
   const smallestSetSize = 5;
+  const [gameActive, setGameActive] = useState(false);
   const [sizeOfSet, setSizeOfSet] = useState(smallestSetSize);
   const [cardOrder, setCardOrder] = useState(newSet(sizeOfSet));
   const [clickedCards, setClickedCards] = useState([]);
@@ -44,22 +45,79 @@ export default function App() {
     setClickedCards([]);
   }
 
-  function restartGame() {
-    setSizeOfSet(smallestSetSize);
-    setCardOrder(newSet(sizeOfSet));
+  function restartGame(size = 5) {
+    setSizeOfSet(size);
+    setCardOrder(newSet(size));
     setClickedCards([]);
     setScore(0);
+  }
+
+  function AllCards() {
+    return (
+      <div className="allCards">
+        {cardOrder.map((index) => (
+          <Card key={index} id={index} handleClick={handleClick} />
+        ))}
+      </div>
+    );
+  }
+
+  function ChooseDifficulty({ handleChooseDifficulty }) {
+    return (
+      <>
+        <h2>Choose the difficulty:</h2>
+        <div className="difficultySelection">
+          <button
+            data="easy"
+            className="difficulty"
+            onClick={handleChooseDifficulty}
+          >
+            Easy
+          </button>
+          <button
+            data="medium"
+            className="difficulty"
+            onClick={handleChooseDifficulty}
+          >
+            Medium
+          </button>
+          <button
+            data="hard"
+            className="difficulty"
+            onClick={handleChooseDifficulty}
+          >
+            Hard
+          </button>
+          <button
+            data="einstein"
+            className="difficulty"
+            onClick={handleChooseDifficulty}
+          >
+            Albert Einstein
+          </button>
+        </div>
+      </>
+    );
+  }
+
+  function handleChooseDifficulty(e) {
+    const difficulty = e.currentTarget.getAttribute('data');
+    setGameActive(true);
+    if (difficulty === 'easy') restartGame(5);
+    else if (difficulty === 'medium') restartGame(10);
+    else if (difficulty === 'hard') restartGame(15);
+    else if (difficulty === 'einstein') restartGame(20);
   }
 
   return (
     <>
       <div className="appContainer">
         <h1>POKéMON MEMORY GAME</h1>
-        <div className="allCards">
-          {cardOrder.map((index) => (
-            <Card key={index} id={index} handleClick={handleClick} />
-          ))}
-        </div>
+        {gameActive ? (
+          <AllCards />
+        ) : (
+          <ChooseDifficulty handleChooseDifficulty={handleChooseDifficulty} />
+        )}
       </div>
       <div className="scores">
         <h3>HIGH SCORE: {highScore}</h3>
